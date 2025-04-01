e30794def3c9cfa9133559d853f0fbeec4ad68ff
PHM-dung2 <pp4992@naver.com>
2025-04-01 11:30:28 +0900

    day1 JPA 어노테이션 실습

---
+++ src/main/java/example/_엔티티/ExamEntity2.java
@@ -0,0 +1,21 @@
+package example._엔티티;
+
+import jakarta.persistence.*;
+
+@Entity // 해당 클래스를 DB테이블과 매핑 관계 주입( 영속성 컨텍스트 저장 )
+@Table( name = "Exam2" ) // DB테이블명 정의, 생략시 클래스명으로 정의된다.
+public class ExamEntity2 {
+    @Id // primary key 제약조건 정의
+    @GeneratedValue( strategy = GenerationType.IDENTITY ) // 생성된 값
+    private long id;
+
+    @Column( nullable = true, unique = false )
+    private String col1;
+
+    @Column( nullable = false, unique = true )
+    private String col2;
+
+    @Column( columnDefinition = "longtext" )
+    private String col3;
+
+}
